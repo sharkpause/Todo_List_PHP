1243fd15c1b0151bb61f9efb8b65548162e42c22
root <carlmabfredishere@gmail.com>
2024-09-02 18:19:23 +0700

    Added project files

---
+++ addTask.php
@@ -0,0 +1,28 @@
+<?php
+
+if($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+	require('./connect.php');
+	
+	if(!$conn) {
+	  die('Connection failed: ' . $conn->connect_error);
+	}
+	
+	if(isset($_POST['task'])) {
+		$taskName = $_POST['task'];
+		$sql = "INSERT INTO tasks (`name`) VALUES ('$taskName')";
+	
+		$query = mysqli_query($conn, $sql);
+		if($query) {
+			header('Location: ./index.php');
+		} else {
+			echo 'Error while attempting the SQL query: ' . $sql . ': ';
+		}
+	
+		die();
+	}
+
+	mysqli_close($conn);
+}
+
+?>
---
+++ connect.php
@@ -0,0 +1,10 @@
+<?php
+
+$servername = 'localhost';
+$username = 'root';
+$password = '';
+$dbname = 'todo_list';
+	
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+?>
